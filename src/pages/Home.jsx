b51415a6d13a0270b21ea4623f2dce4c97108d09
Armin1723 <110764802+Armin1723@users.noreply.github.com>
2024-06-10 00:18:45 +0530

    fix: build CSS issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,7 @@ const Home = () => {
 
   const fetchSuggestions = async (e) => {
     setBookName(e.target.value);
+    setResults([]);
     try {
       if (bookName.length > 2) {
         setIsLoading(true);
@@ -34,12 +35,14 @@ const Home = () => {
   };
 
   const fetchResults = async (bookName) => {
+    setIsResultsLoading(true);
     try {
       // fetch results
-      const result = await fetch(
-        `https://openlibrary.org/search.json?title=${bookName}&fields=title&limit=5&page=1`
+      const data = await fetch(
+        `https://openlibrary.org/search.json?title=${bookName}&fields=key,title,author_name,editions&limit=10&page=1`
       ).then((res) => res.json());
-      console.log(result.docs);
+      setResults(data.docs);
+      setIsResultsLoading(false);
     } catch (error) {
       console.log(error.message);
     }
@@ -47,7 +50,7 @@ const Home = () => {
 
   return (
     <motion.div
-      className={` rounded-t-2xl min-h-screen flex flex-col items-center justify-start mt-2 pt-[20vh] ${formatSectionBackground(
+      className={` rounded-t-2xl min-h-screen flex flex-col items-center justify-start mt-2 pt-[10vh] ${formatSectionBackground(
         theme
       )}`}
       initial={{ y: 48, opacity: 0.2 }}
@@ -72,10 +75,12 @@ const Home = () => {
             onChange={(e) => fetchSuggestions(e)}
             className="border-2 rounded-l-xl border-gray-500/70 bg-inherit focus:border-0 ring-0 ring-none p-2 w-[70vw] md:w-[50vw] "
           />
-          <button className="flex items-center border-2 border-gray-500/70 bg-inherit border-l-0 rounded-r-xl px-2 hover:bg-gray-300/40">
+          <button disabled={results.length > 0} className="flex items-center border-2 border-gray-500/70 bg-inherit border-l-0 rounded-r-xl px-2 hover:bg-gray-300/40">
             Search
           </button>
         </div>
+
+        {/* Suggestions */}
         {isLoading ? <p className="pl-4 py-2">Loading...</p> : (suggestions &&
           suggestions.map((suggestion, index) => (
             <button
@@ -90,9 +95,28 @@ const Home = () => {
                 index === 0 && "border-t-0 -mt-1"
               } max-w-full px-4 py-2 border border-gray-500/70 ${!suggestion.noResults && 'hover:bg-gray-500/30'} text-left `}
             >
-              {suggestion.title}
+              {suggestion.title.length > 40 ? suggestion.title.slice(0, 40) + "..." : suggestion.title}
             </button>
           )))}
+
+        {/* Results */}
+        <div className="max-h-[40vh] md:max-w-[54vw] max-w-[85vw] overflow-auto">
+        {isResultsLoading ? <p className="pl-4 py-2">Loading...</p> : (results &&
+          results.map((result, index) => (
+            <div
+              key={index}
+              className={` ${
+                index === 0 && "border-t-0 -mt-2"
+              } max-w-full px-4 py-2 border-b border-gray-500/70 text-left `}
+            >
+              <p className="text-lg font-bold">"{result.title}"</p>
+              <p className="text-sm font-light">Author: &nbsp;
+                {result.author_name ? result.author_name.join(", ") : "Unknown"}
+              </p>
+              <p className="text-sm font-light">Number of Editions: {result.editions.numFound}</p>
+            </div>
+          )))}
+        </div>
       </div>
     </motion.div>
   );
